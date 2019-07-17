09e57d0921652fa7418568fe69275ef31286b457
maeta <nexus18789@gmail.com>
2019-07-17 20:15:45 +0900

    ログインエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by(email: params[:session][:email]
-    # if user && user.authenticate(params[:session][:password])
     user = User.find_by(email_params)
     if user && user.authenticate(password_params[:password])
       log_in user
@@ -21,8 +19,17 @@ class SessionsController < ApplicationController
   end
 
   private
+
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def email_params
+    params.require(:session).permit(:email)
+  end
+
+  def password_params
+    params.require(:session).permit(:password)
   end
 
   def log_out
